775b89d0027cdd39d5b3eee1f25402933f7a7d37
Ruslan Kornev <oganer@gmail.com>
2020-05-18 03:59:48 +0300

    Draft version

--- lib/navag/commands/config.rb
+++ lib/navag/commands/config.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 require_relative '../command'
+require 'tty/tree'
+require 'tty/reader'
+require 'tty-prompt'
+require 'tty-editor'
+require 'active_support'
+require 'active_support/core_ext/string/filters'
+require 'ostruct'
 
 module Navag
   module Commands
@@ -10,8 +17,58 @@ module Navag
       end
 
       def execute(input: $stdin, output: $stdout)
-        # Command logic goes here ...
-        output.puts "OK"
+        prompt = TTY::Prompt.new
+        print prompt.cursor.clear_screen
+        print prompt.cursor.move_to(0, 0)
+        @current_path = ['.']
+        loop do
+          branch = tree.dig(*@current_path)
+          if branch.is_a?(Hash)
+            answer = prompt.select(nil, branch.keys.sort, per_page: 40, cycle: true, filter: true)
+            selected_name = answer.inspect.delete('\'"')
+            @current_path << selected_name
+            print prompt.cursor.clear_screen
+            print prompt.cursor.move_to(0, 0)
+            puts @current_path.join('/')
+          else
+            if @current_path[-1] == '..'
+              @current_path.pop(2)
+              print prompt.cursor.clear_screen
+              print prompt.cursor.move_to(0, 0)
+              puts @current_path.join('/')
+            else
+              print tree.dig(*@current_path).text.squish
+              # TTY::Editor.open(content: tree.dig(*@current_path).text.squish)
+              # TTY::Editor.open(content: tree.dig(*@current_path).text.squish)
+              @current_path.pop
+            end
+          end
+        end
+      end
+
+      private
+
+      def tree
+        return @tree if defined?(@tree)
+
+        @tree = { '.' => {} }
+        File.open('result.txt').each_line do |line|
+          match = /(?<path>.+?):(?<line>.+?):(?<column>.+?):(?<text>.+)/.match(line)
+          data = OpenStruct.new({
+              line: match['line'],
+              column: match['column'],
+              text: match['text']
+          })
+          splitted = match['path'].split('/')
+          @tree['.'].deep_merge!(undig(*splitted, data))
+        end
+        @tree
+      end
+
+      def undig(*keys, value)
+        keys[0..-2].reverse_each.reduce ({ keys.last=>value, '..' => nil }) do |h, key|
+          { key => h, '..' => nil }
+        end
       end
     end
   end
--- navag.gemspec
+++ navag.gemspec
@@ -7,16 +7,14 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Ruslan Kornev"]
   spec.email         = ["oganer@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Handy shell for ag results.}
+  spec.description   = %q{Handy shell for ag results.}
+  spec.homepage      = "https://github.com/woto/agx"
   spec.required_ruby_version = Gem::Requirement.new(">= 2.3.0")
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
-
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = "https://github.com/woto/agx"
+  spec.metadata["changelog_uri"] = "https://github.com/woto/agx/CHANGELOG.md"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -26,4 +24,10 @@ Gem::Specification.new do |spec|
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
+
+  spec.add_dependency "tty-tree"
+  spec.add_dependency "tty-prompt"
+  spec.add_dependency "tty-cursor"
+  spec.add_dependency "tty-editor"
+  spec.add_dependency "activesupport"
 end
